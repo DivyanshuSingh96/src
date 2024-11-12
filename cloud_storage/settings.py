fc49823b338fc941875899e0e365174a28b60893
Divyanshu Singh <79963506+DivyanshuSingh96@users.noreply.github.com>
2024-11-12 21:38:53 +0530

    Update settings.py

--- cloud_storage/settings.py
+++ cloud_storage/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = env_config("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env_config("DJANGO_DEBUG", cast=bool, default=False)
 
-ALLOWED_HOSTS = [".vercel.app"]
+ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app"]
 
 
 # Application definition
